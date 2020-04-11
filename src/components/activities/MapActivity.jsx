f108f41dbba2e20eb75274ceed5052c2b2d3036c
Bernat <bernatesquirol@gmail.com>
2020-04-11 18:43:04 +0200

    map activity

---
+++ src/components/activities/MapActivity.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import InteractiveMap, {Marker, MapController} from 'react-map-gl';
+import useDimensions from "react-use-dimensions";
+
+const getActivityObject = ({activityId, userId}, value)=>{
+    return {
+        activityId,
+        userId,
+        value,
+        createdAt: Date.now()
+    }
+}
+
+const getIfAnswerIsCorrect = ({correctAnswer},activityObject)=>{
+    return correctAnswer==activityObject.value
+}
+
+const defaultViewport=()=>{
+    return {
+        bearing: -44.4105,
+        latitude: 41.3870154,
+        longitude: 2.1700471,
+        zoom: 12
+    }
+}
+
+function MapActivity (props) {
+    const {updateFunction, activityEvents, correctAnswer, userControlled, userId, activityId} = props
+    const latestObject = activityEvents && activityEvents.length>0? activityEvents[activityEvents.length-1]:{}
+    const [ref, { x, y, width, height }] = useDimensions();
+    const viewport = latestObject.value?{ width:width, height:height,...latestObject.value }: { width:width, height:height,...defaultViewport()}
+    if (latestObject.value){
+        console.log('recieved something')
+    }
+    const handleUpdate = (objV)=>{
+        if (userControlled){
+            updateFunction(objV)
+        }
+    }
+    
+    return (<>
+            <div ref={ref} style={{width:'100%', height:window.innerHeight-35}}>
+            {width&&height?<InteractiveMap
+                // onClick={(e)=>{
+                //     saveMarker([...markers, {longitude:e.lngLat[0], latitude:e.lngLat[1], from: 8, value: 8 }])
+                // }}
+                mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+                // mapStyle={'mapbox://styles/bernatesquirol/ck82o72do39201iod1pno8huk'}
+                {...viewport}
+                onViewportChange={({bearing, latitude, longitude, zoom})=>{handleUpdate(getActivityObject(props, {bearing, latitude, longitude, zoom}))}}
+                >
+                </InteractiveMap>
+                :null}
+            </div>
+            </>)
+}
+
+export default MapActivity
--- src/components/activitiesEnum.js
+++ src/components/activitiesEnum.js
@@ -1,19 +1,23 @@
 import InputActivity from './activities/InputActivity'
 import { Form } from 'semantic-ui-react'
 import CanvasActivity from './activities/CanvasActivity'
+import MapActivity from './activities/MapActivity'
 export const activityNames = {
     'inputActivity': "Text amb resposta correcta",
-    'canvasActivity': "Pintar"
+    'canvasActivity': "Pintar",
+    'mapActivity': "Mapa"
 }
 export const activityComponents = {
     'inputActivity': InputActivity,
-    'canvasActivity': CanvasActivity
+    'canvasActivity': CanvasActivity,
+    'mapActivity': MapActivity
 }
 
 export const activityExtraFields = {
     'inputActivity': {
         'correctAnswer': {'element':Form.Input, 'label': 'Resposta correcta'}
     },
-    'canvasActivity':{}
+    'canvasActivity':{},
+    'mapActivity':{}
 
 }
